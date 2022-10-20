4a6255185a988ffaed05ffdfb8c995d50ad93869
adb-sh <git@adb.sh>
2022-10-21 01:16:33 +0200

    catch error when loading theme config

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,16 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import { createApi } from "@/Api";
-import 'bootstrap-icons/font/bootstrap-icons.scss';
-import './main.scss';
-import { ThemeConfig } from 'bootstrap-darkmode';
+import "bootstrap-icons/font/bootstrap-icons.scss";
+import "./main.scss";
+import { ThemeConfig } from "bootstrap-darkmode";
 
 export const themeConfig = new ThemeConfig();
-themeConfig.initTheme();
+try {
+  themeConfig.initTheme();
+} catch (e) {
+  console.error('failed to init theme');
+}
 
 createApp(App)
   .use(router)
